c2f1207d26ac3090564d5ef18243eecd6300ecc3
tyomakus <pochta_451@mail.ru>
2023-04-19 00:37:29 +0700

    New features, functions, design...

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class SurveyApplication(tk.Tk):
         self.create_widgets()
         self.question_index = 0
         self.questions = []
+        self.result_counter = 0
 
     def create_widgets(self):
         # Создаем метку для заголовка опроса
@@ -36,11 +37,16 @@ class SurveyApplication(tk.Tk):
 
         # Создаем кнопку для отправки ответа
         self.submit_button = tk.Button(self, text="Отправить", command=self.submit_answer)
-        self.submit_button.pack()
+        #self.submit_button.pack()
 
         #Создаем кнопку для подтверждения оконченности создания опроса
         self.submit_quiz_button = tk.Button(self, text = "Подтвердить", command=self.submit_quiz)
         self.submit_quiz_button.pack()
+
+    def result_count(self):
+    #Считаем общий балл
+        result_count_label = tk.Label(self, text = ("Общий балл: " + str(self.result_counter)))
+        result_count_label.pack(side=tk.BOTTOM)
 
     def add_question(self):
         # Получаем текст вопроса и ответа
@@ -58,12 +64,17 @@ class SurveyApplication(tk.Tk):
     def submit_answer(self):
         # Получаем ответ на вопрос
         answer = self.answer_entry.get()
-
         # Обрабатываем ответ на вопрос
         # Здесь можно сохранять ответы в базу данных или файл
         question = self.questions[self.question_index]
         is_correct = answer.lower() == question['answer'].lower()
+        if is_correct:
+            self.result_counter += 1
+        else:
+            self.result_counter -= 1
         self.show_result(is_correct)
+        
+        
 
         # Очищаем поле для ввода ответа
         self.answer_entry.delete(0, tk.END)
@@ -75,15 +86,41 @@ class SurveyApplication(tk.Tk):
             self.title_label.config(text="Опрос")
             self.question_label.config(text=question['question'])
         else:
-            self.title_label.config(text="Опрос завершен")
+            self.title_label.config(text="Опрос завершен", font=(14))
+            self.answer_entry.pack_forget()
+            self.answer_label.pack_forget()
+            self.question_label.pack_forget()
+            self.submit_button.pack_forget()
+            self.result_count()
+
         
     def submit_quiz(self):
-            pass
+        # Очищаем поля для ввода вопроса и ответа
+            self.question_entry.delete(0, tk.END)
+            self.answer_entry.delete(0, tk.END)
+        #Удаляем кнопку
+            self.submit_quiz_button.destroy()
+        #Добавляем кнопку ответа
+            self.submit_button.pack()
+        #Выводим вопрос
+            question = self.questions[self.question_index]
+            self.question_label.config(text=question['question'])
+        #Удаляем лишние кнопочки
+            self.question_entry.pack_forget()
+            self.add_question_button.pack_forget()
+            self.submit_quiz_button.destroy()
+
 
     def show_result(self, is_correct):
         # Создаем метку для вывода результата
-        result_text = "Верно!" if is_correct else "Неверно!"
-        result_label = tk.Label(self, text=result_text)
+        question = self.questions[self.question_index]
+        self.question_num_text = (str(self.question_index+1) + ". " + str(question['question']))
+        if is_correct:
+            result_text = self.question_num_text + ". " + "Верно!"
+            
+        else:
+            result_text = str(self.question_index+1) + ". " "Неверно!"
+        result_label = tk.Label(self, text = result_text)
         result_label.pack()
 
 if __name__ == '__main__':
