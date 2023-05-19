84e45a3a7d0d1a0763046cab7ce50279416a1973
tyomakus <pochta_451@mail.ru>
2023-05-19 23:34:00 +0700

    new tests

--- test.py
+++ test.py
@@ -4,10 +4,13 @@ from main import SurveyApplication
 class TestMain(unittest.TestCase):
     def setUp(self):
         self.SurveyApplication = SurveyApplication()
-    #Each test method starts with the keyword test_
     def test_open_file(self):
-        questions_txt = SurveyApplication.open_file(question_txt)
-        self.assertNotEqual(questions_txt, 0)
+        filepath_test = SurveyApplication.filepath
+        self.assertNotEqual(filepath_test, "")
+    def test_geometry(self):
+        geom = SurveyApplication.geometry
+        self.assertEqual(geom, "500x500")
+
     # Executing the tests in the above test case class
 if __name__ == "__main__":
   unittest.main()
