f666b148d736e7182f2be9296bc6d98b303caaf9
tyomakus <pochta_451@mail.ru>
2023-05-20 20:06:15 +0700

    Fix marking

--- test.py
+++ test.py
@@ -4,12 +4,15 @@ from main import SurveyApplication
 class TestMain(unittest.TestCase):
     def setUp(self):
         self.SurveyApplication = SurveyApplication()
-    def test_open_file(self):
-        filepath_test = SurveyApplication.filepath
-        self.assertNotEqual(filepath_test, "")
+    #def test_open_file(self):
+        #filepath_test = SurveyApplication.filepath
+        #self.assertNotEqual(filepath_test, "")
     def test_geometry(self):
         geom = SurveyApplication.geometry
-        self.assertEqual(geom, "500x500")
+        self.assertNotEqual(geom, "500x500")
+    def width_test(self):
+        width =  SurveyApplication.width_question
+        self.assertTrue(width <= 500)
 
     # Executing the tests in the above test case class
 if __name__ == "__main__":
